- provided a better cross platform file path for test_frame_process - fixed the normalization (I think...) using boolean masking, which isn't really normalization anymore - added checks to make sure Tensorflow is using the GPU

Space Invaders - Deep Q Learning/main.py
@@ -18,9 +18,11 @@ if __name__ == "__main__":
     logging.config.fileConfig('logging.conf', defaults={'logfile': logfile})
     logging.debug("Starting main")
     warnings.filterwarnings('ignore')
+    training_params = TrainingParams()
+    if training_params.gpu_required and not tf.test.is_gpu_available() and not tf.test.is_built_with_cuda():
+        raise Exception("GPU is required and Tensorflow has no GPU available.")
     trainer = Trainer()
     environment = Environment()
-    training_params = TrainingParams()
     player = Player.create_class_with_param_object(training_params=training_params,
                                                    possible_actions=environment.get_possible_actions(),
                                                    action_size=environment.get_action_size())

Space Invaders - Deep Q Learning/processing/frame_process.py
@@ -1,7 +1,5 @@
 from skimage.color import rgb2gray
 from skimage import transform
-import numpy as np
-from sklearn import preprocessing
 import logging
 
 
@@ -19,18 +17,15 @@ class FramePreprocessor(object):
         return rgb2gray(frame)
 
     def crop_frame(self, frame):
-        # return frame[self.top_crop: frame.shape[0] - self.bottom_crop, self.left_crop: frame.shape[1] - self.right_crop]
         return frame[self.top_crop: -self.bottom_crop, self.left_crop: -self.right_crop]
 
-    # def normalize_frame(self, frame):
-    #     # if self.normalize > 0:
-    #     #     return frame/self.normalize
-    #     # else:
-    #     #     return frame
-    #     x = np.random.rand(1000)*10
-    #     norm1 = x / np.linalg.norm(x)
-    #     norm2 = preprocessing.normalize(x[:,np.newaxis], axis=0).ravel()
-
+    def normalize_frame(self, frame):
+        if self.normalize > 0:
+            normalized_frame = frame/self.normalize
+            normalized_frame = normalized_frame > 0.0001
+            return normalized_frame.astype(int)
+        else:
+            return frame
 
     def resize_frame(self, frame):
         if self.resize_height > 0 and self.resize_width > 0:
@@ -41,9 +36,8 @@ class FramePreprocessor(object):
     def preprocess_frame(self, frame):
         gray_frame = self.convert_grayscale(frame)
         cropped_frame = self.crop_frame(gray_frame)
-        # normalized_frame = self.normalize_frame(cropped_frame)
-        # return self.resize_frame(normalized_frame)
-        return self.resize_frame(cropped_frame)
+        resized_frame = self.resize_frame(cropped_frame)
+        return self.normalize_frame(resized_frame)
 
     @classmethod
     def preprocess_frame_class(cls, env_object, frame):

Space Invaders - Deep Q Learning/processing/tests/test_frame_process.py
@@ -1,10 +1,14 @@
 import nose
+import numpy as np
+import os
 import retro
 import imageio
 from processing.frame_process import FramePreprocessor
 
 
 RETRO_GAME = 'SpaceInvaders-Atari2600'
+CUR_FILE_PATH = os.path.dirname(os.path.abspath(__file__))
+IMAGE_PATH = os.path.join(CUR_FILE_PATH, "images")
 
 
 def get_retro_frame():
@@ -27,7 +31,7 @@ def test_frameprocessor_convert_grayscale():
     expected_shape = (frame.shape[0], frame.shape[1])
     print(grayscale_frame.shape)
     print(expected_shape)
-    imageio.imsave("./images/test_grayscale_frame.png", grayscale_frame)
+    save_image(grayscale_frame, "test_grayscale_frame.png")
     assert expected_shape == grayscale_frame.shape
 
 
@@ -40,12 +44,17 @@ def test_frameprocessor_crop_frame():
     assert cropped_frame.shape == (190, 144, 3)
 
 
-# def test_frameprocessor_normalize_frame():
-#     frame = get_retro_frame()
-#     frame_processor = FramePreprocessor(None, None, None, None, 255.0, None, None)
-#     frame = frame_processor.convert_grayscale(frame)
-#     normalized_frame = frame_processor.normalize_frame(frame)
-#     imageio.imsave("./images/test_normalized_frame.png", normalized_frame)
+def test_frameprocessor_normalize_frame():
+    frame = get_retro_frame()
+    frame_processor = FramePreprocessor(8, 12, 4, 12, 255.0, None, None)
+    frame = frame_processor.convert_grayscale(frame)
+    frame = frame_processor.crop_frame(frame)
+    normalized_frame = frame_processor.normalize_frame(frame)
+    frame_min = np.amin(normalized_frame)
+    frame_max = np.amax(normalized_frame)
+    assert frame_min == 0
+    assert frame_max == 1
+    save_image(normalized_frame, "test_normalized_frame.png")
 
 
 def test_frameprocessor_resize_frame():
@@ -62,5 +71,19 @@ def test_frameprocessor_preprocess_frame():
     frame = get_retro_frame()
     frame_processor = FramePreprocessor(8, 12, 4, 12, 255.0, 110, 84)
     processed_frame = frame_processor.preprocess_frame(frame)
-    imageio.imsave("./images/test_processed_frame.png", processed_frame)
+    save_image(processed_frame, "test_processed_frame.png")
+    frame_min = np.amin(processed_frame)
+    frame_max = np.amax(processed_frame)
+    print("min: {}, max: {}".format(frame_min, frame_max))
+    assert frame_min == 0
+    assert frame_max == 1
     assert processed_frame.shape == (110, 84)
+
+
+
+def save_image(frame, filename):
+    if not os.path.exists(IMAGE_PATH):
+        os.mkdir(IMAGE_PATH)
+    frame_path = os.path.join(IMAGE_PATH, filename)
+    print(frame_path)
+    imageio.imsave(frame_path, frame)

Space Invaders - Deep Q Learning/trainer/trainer.py
@@ -6,7 +6,7 @@ tf.disable_v2_behavior()
 
 class Trainer(object):
     def train_model(self, environment, player, training_params):
-        with tf.Session() as sess:
+        with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
             sess.run(tf.global_variables_initializer())
             if training_params.use_existing_model:
                 player.load_model(sess)