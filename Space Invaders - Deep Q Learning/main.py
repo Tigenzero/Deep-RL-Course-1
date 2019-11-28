First Cartpole - Policy Gradient Commit - other projects: changed folder names to prevent importing the wrong packages - created all params and classes to train the model. Will implement testing later

Space Invaders - Deep Q Learning/main.py
@@ -4,7 +4,7 @@ import warnings
 import tensorflow.compat.v1 as tf
 from player.player import Player
 from deep_q_params import TrainingParams
-from trainer.trainer import Trainer
+from SI_trainer.trainer import Trainer
 from environment.environment import Environment
 tf.disable_v2_behavior()
 